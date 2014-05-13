f494493e8a957f813af5a03f21dd50aa5fe7cb0e
eldiablosab <sabil.djerir@yahoo.fr>
2014-05-13 22:47:53 +0200

    new project

---
+++ src/NewProject.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+/**
+ * Created by yoyonice on 13/05/2014.
+ */
+public class NewProject {
+    private JButton createButton;
+    private JFormattedTextField nameFormattedTextField;
+    private JTextField createANewProjectTextField;
+    private JFormattedTextField beginDateFormattedTextField;
+    private JFormattedTextField finishDateFormattedTextField;
+}
