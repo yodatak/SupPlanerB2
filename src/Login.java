aa5717a7f161e61f778ffff34b444bb9faf9a7de
yodatak <mryodatak@gmail.com>
2014-05-13 21:58:25 +0200

    Login add

--- src/Login.java
+++ src/Login.java
@@ -1,18 +1,37 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
 
 /**
  * Created by yodatak on 13/05/14.
  */
-public class Login {
+public class Login extends JFrame{
     private JButton registerButton;
     private JButton loginButton;
     private JFormattedTextField myemailGmailComFormattedTextField;
     private JPasswordField passwordPasswordField;
     private JPanel Login;
 
+    public Login() {
+        super("Login");
+        this.setContentPane(Login);
+
+        final Login capture_this = this;
+
+        registerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame frame = new Register();
+                frame.pack();
+                frame.setVisible(true);
+                capture_this.setVisible(false);
+            }
+        });
+    }
+
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Login");
-        frame.setContentPane(new Login().Login);
+        JFrame frame = new Login();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
---
+++ src/Register.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by yodatak on 13/05/14.
+ */
+public class Register extends JFrame {
+    private JFormattedTextField formattedTextField1;
+    private JPasswordField passwordField1;
+    private JPasswordField passwordField2;
+    private JRadioButton employeeRadioButton;
+    private JRadioButton managerRadioButton;
+    private JButton validateButton;
+    public JPanel Register;
+
+    public Register() throws HeadlessException {
+        super("Register");
+        setContentPane(Register);
+    }
+}
