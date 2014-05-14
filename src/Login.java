411731cd865bc12d24c2e41fbc0751b07cee1e0e
germinolegrand <germinolegrand@gmail.com>
2014-05-14 21:27:29 +0200

    main Login => Main

--- src/Login.java
+++ src/Login.java
@@ -4,6 +4,10 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Formatter;
 
 /**
  * Created by yodatak on 13/05/14.
@@ -39,7 +43,7 @@ public class Login extends JFrame{
                     PrintStream out = sck.out();
                     out.println("CONNECT");
                     out.println(myemailGmailComFormattedTextField.getText());
-                    if(sck.request(new String(passwordPasswordField.getPassword())).equals("ACCEPTED")){
+                    if(sck.request(encryptPassword(new String(passwordPasswordField.getPassword()))).equals("ACCEPTED")){
                         JFrame frame = new DashboardAll();
                         frame.setLocationRelativeTo(null);
                         frame.pack();
@@ -51,5 +55,38 @@ public class Login extends JFrame{
                 }
             }
         });
+    }
+
+    public static String encryptPassword(String password)
+    {
+        String sha1 = "";
+        try
+        {
+            MessageDigest crypt = MessageDigest.getInstance("SHA-1");
+            crypt.reset();
+            crypt.update(password.getBytes("UTF-8"));
+            sha1 = byteToHex(crypt.digest());
+        }
+        catch(NoSuchAlgorithmException e)
+        {
+            e.printStackTrace();
+        }
+        catch(UnsupportedEncodingException e)
+        {
+            e.printStackTrace();
+        }
+        return sha1;
+    }
+
+    private static String byteToHex(final byte[] hash)
+    {
+        Formatter formatter = new Formatter();
+        for (byte b : hash)
+        {
+            formatter.format("%02x", b);
+        }
+        String result = formatter.toString();
+        formatter.close();
+        return result;
     }
 }
--- src/Register.java
+++ src/Register.java
@@ -1,5 +1,11 @@
+import sun.rmi.runtime.Log;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.io.PrintStream;
 
 /**
  * Created by sabil on 13/05/14.
@@ -15,5 +21,29 @@ public class Register extends JFrame {
     public Register() throws HeadlessException {
         super("Register");
         setContentPane(Register);
+
+        final Register capture_this = this;
+
+        validateButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    SocketClient sck = new SocketClient();
+                    PrintStream out = sck.out();
+                    out.println("REGISTER");
+                    out.println(formattedTextField1.getText());
+                    out.println(managerCheckBox.isSelected() ? "MANAGER" : "EMPLOYEE");
+                    if(sck.request(Login.encryptPassword(new String(passwordField1.getPassword()))).equals("ACCEPTED")){
+                        JFrame frame = new DashboardAll();
+                        frame.setLocationRelativeTo(null);
+                        frame.pack();
+                        frame.setVisible(true);
+                        capture_this.setVisible(false);
+                    }
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        });
     }
 }
