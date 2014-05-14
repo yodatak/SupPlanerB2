615255d5024efb868e58b2bfdc3afa52753aa434
germinolegrand <germinolegrand@gmail.com>
2014-05-14 18:08:00 +0200

    ajout de l'action sur le login

--- src/SocketClient.java
+++ src/SocketClient.java
@@ -4,25 +4,16 @@ import java.io.*;
  * Created by yodatak on 13/05/14.
  */
 public class SocketClient {
+    Socket socket;
+    public String request(String req) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintStream out = new PrintStream(socket.getOutputStream());
 
-    public SocketClient() {
-        Socket socket;
-        DataInputStream userInput;
-        PrintStream theOutputStream;
+        out.println(req);
+        return in.readLine();
+    }
 
-        try {
-            InetAddress serveur = InetAddress.getLocalHost();
-            socket = new Socket(serveur, 3333);
-
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintStream out = new PrintStream(socket.getOutputStream());
-
-            out.println("yop");
-            System.out.println(in.readLine());
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
+    public SocketClient() throws IOException {
+        socket = new Socket(InetAddress.getLocalHost(), 3333);
     }
 }
