eb77ab6dc864700998f5144930e25684c6e6d8fe
yodatak <mryodatak@gmail.com>
2014-05-14 18:16:15 +0200

    projectDashBoard y'a des soushis.

--- src/ProjectDashboard.java
+++ src/ProjectDashboard.java
@@ -1,11 +1,18 @@
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by yodatak on 13/05/14.
  */
-public class ProjectDashboard {
+public class ProjectDashboard extends JFrame{
     private JButton backButton;
     private JButton logoutButton;
     private JButton newTaskButton;
     private JButton inviteEmployeeButton;
+    public JPanel ProjectDashboard;
+
+    public ProjectDashboard() throws HeadlessException {
+        super("ProjectDashboard");
+        setContentPane(ProjectDashboard);
+    }
 }
