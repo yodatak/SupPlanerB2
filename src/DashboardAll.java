e5b64d59eb55f7f837e2932ac6b8a01b0395b492
germinolegrand <germinolegrand@gmail.com>
2014-05-14 18:55:06 +0200

    ajout de l'action sur le login

--- src/DashboardAll.java
+++ src/DashboardAll.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
 
 /**
  * Created by yodatak on 13/05/14.
@@ -12,5 +15,15 @@ public class DashboardAll extends JFrame {
     public DashboardAll(){
         super("Dashboard For All Projects");
         setContentPane(Dash);
+
+        final DashboardAll capture_this = this;
+
+        logoutButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                capture_this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                capture_this.processWindowEvent(new WindowEvent(capture_this, WindowEvent.WINDOW_CLOSING));
+            }
+        });
     }
 }
